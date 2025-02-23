78d7b19fb80a08031dd419217834ba5f2bfdad5a
morronepablo <morronepablo@gmail.com>
2025-02-23 11:26:52 -0300

    Desarrollo de Gestión Lotes, parte 3 - Sistema Farmacia PHP JS MYSQL HTML CSS

--- Controllers/MovimientoController.php
+++ Controllers/MovimientoController.php
@@ -47,12 +47,43 @@ if ($_POST['funcion'] == 'ver_detalle') {
 	echo json_encode($json);
 } elseif ($_POST['funcion'] == 'obtener_lotes') {
 	$movimiento->obtener_lotes();
-	var_dump($movimiento->objetos);
-	/*
-	$json = array(
-		'mensaje'	=>	$mensaje,
-	);
+	$json = array();
+	$fecha_actual = date('Y-m-d H:i:s');
+	$fecha_actual = new DateTime($fecha_actual);
+	foreach ($movimiento->objetos as $objeto) {
+		$vencimiento = new DateTime($objeto->fecha_vencimiento);
+		$diferencia = $vencimiento->diff($fecha_actual);
+		$year = $diferencia->y;
+		$mes = $diferencia->m;
+		$dia = $diferencia->d;
+		$verificado = $diferencia->invert;
+		$estado = 'light';
+		if ($verificado == 0) {
+			$estado = 'danger';
+		} else {
+			if ($mes > 5) {
+				$estado = 'light';
+			}
+			if ($mes <= 5 && $year == 0) {
+				$estado = 'warning';
+			}
+		}
+		$json[] = array(
+			'id' 			=> openssl_encrypt($objeto->id, CODE, KEY),
+			'compra' 		=> $objeto->compra,
+			'cantidad_res' 	=> $objeto->cantidad_res,
+			'cantidad' 		=> $objeto->cantidad,
+			'precio_compra' => $objeto->precio_compra,
+			'lote' 			=> $objeto->lote,
+			'vencimiento' 	=> $objeto->fecha_vencimiento,
+			'producto' 		=> str_replace("***", "%", $objeto->producto),
+			'concentracion' => str_replace("***", "%", $objeto->concentracion),
+			'laboratorio' 	=> $objeto->laboratorio,
+			'subtipo' 		=> $objeto->subtipo,
+			'presentacion' 	=> $objeto->presentacion,
+			'estado' 		=> $estado
+		);
+	}
 
 	echo json_encode($json);
-	*/
 }
