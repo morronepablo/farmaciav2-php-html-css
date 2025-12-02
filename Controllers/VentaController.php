a245bd3bde63e4f6d93f979a9c2ee721280e0bd0
morronepablo <morronepablo@gmail.com>
2025-12-02 11:35:17 -0300

    258. Desarrollo de ventas, parte 18 - Sistema Farmacia PHP JS MYSQL HTML CSS

--- Controllers/VentaController.php
+++ Controllers/VentaController.php
@@ -1,27 +1,32 @@
 <?php
-include_once $_SERVER["DOCUMENT_ROOT"] . '/farmaciav2/Models/EstadoPago.php';
+include_once $_SERVER["DOCUMENT_ROOT"] . '/farmaciav2/Models/Movimiento.php';
+include_once $_SERVER["DOCUMENT_ROOT"] . '/farmaciav2/Models/Venta.php';
 include_once $_SERVER["DOCUMENT_ROOT"] . '/farmaciav2/Util/Config/config.php';
-$estado_pago = new EstadoPago();
+$movimiento = new Movimiento();
+$venta 		= new Venta();
 session_start();
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 $fecha_actual = date('d-m-Y');
 
 if ($_POST['funcion'] == 'registrar_venta') {
-	$datos 			= json_decode($_POST['datos']);
-	$cliente 		= $datos->cliente;
-	$formateado		= str_replace(' ', '+', $cliente);
-	$cliente		= openssl_decrypt($formateado, CODE, KEY);
-	$comprobante	= $datos->comprobante;
-	$formateado		= str_replace(' ', '+', $comprobante);
-	$comprobante	= openssl_decrypt($formateado, CODE, KEY);
-	$descuento 		= $datos->descuento;
-	$grabada 		= $datos->grabada;
-	$iva 			= $datos->iva;
-	$total 			= $datos->total;
-	$recibe 		= $datos->recibe;
-	$cambio 		= $datos->cambio;
-	$productos 		= $datos->productos;
-	$mensaje		= '';
+	$usuario			= $_SESSION['id'];
+	$datos 				= json_decode($_POST['datos']);
+	$cliente 			= $datos->cliente;
+	$formateado			= str_replace(' ', '+', $cliente);
+	$cliente			= openssl_decrypt($formateado, CODE, KEY);
+	$comprobante		= $datos->comprobante;
+	$formateado			= str_replace(' ', '+', $comprobante);
+	$comprobante		= openssl_decrypt($formateado, CODE, KEY);
+	$descuento 			= $datos->descuento;
+	$grabada 			= $datos->grabada;
+	$iva 				= $datos->iva;
+	$total 				= $datos->total;
+	$recibe 			= $datos->recibe;
+	$cambio 			= $datos->cambio;
+	$productos 			= $datos->productos;
+	$mensaje			= '';
+	$mensaje_productos	= '';
+	$bandera			= 0;
 	//var_dump($cliente, $comprobante, $descuento, $grabada, $iva, $total, $recibe, $cambio, $productos);
 	if (is_numeric($cliente) && is_numeric($comprobante)) {
 		foreach ($productos as $producto) {
@@ -29,11 +34,25 @@ if ($_POST['funcion'] == 'registrar_venta') {
 			$formateado		= str_replace(' ', '+', $producto->id);
 			$id_producto	= openssl_decrypt($formateado, CODE, KEY);
 			@$producto->id	= $id_producto;
+			$stock_real		= 0;
+			$movimiento->obtener_stock_sin_vencer($id_producto);
+			if (!empty($movimiento->objetos[0]->total)) {
+				$stock_real = $movimiento->objetos[0]->total;
+			}
+			if ($stock_real < $cantidad) {
+				$mensaje_productos .= 'El producto ' . $producto->nombre . ' tiene ' . $stock_real . ' de stock. ';
+				$bandera++;
+			}
 		}
-		var_dump($productos);
-		$mensaje = 'success';
+		if ($bandera == 0) {
+			//$venta->crear($grabada, $descuento, $iva, $total, $comprobante, $cliente);
+			var_dump($grabada, $descuento, $iva, $total, $comprobante, $cliente);
+			$mensaje = 'success';
+		} else {
+			$mensaje = 'error_cantidad_falsa';
+		}
 	} else {
 		$mensaje = 'error_decrypt';
 	}
-	echo json_encode(array('mensaje' => $mensaje));
+	echo json_encode(array('mensaje' => $mensaje, 'mensaje_producto' => $mensaje_productos));
 }
---
+++ Models/Venta.php
@@ -0,0 +1,26 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . '/farmaciav2/Models/Conexion.php';
+class Venta
+{
+	var $objetos;
+	var $acceso;
+	public function __construct()
+	{
+		$db = new Conexion();
+		$this->acceso = $db->pdo;
+	}
+
+	function crear($descripcion, $total, $proveedor)
+	{
+		$sql = "CALL crear_obtener_id_pedido(:proveedor,:descripcion,:total)";
+		$variables = array(
+			':descripcion' => $descripcion,
+			':total' => $total,
+			':proveedor' => $proveedor,
+		);
+		$query = $this->acceso->prepare($sql);
+		$query->execute($variables);
+		$this->objetos = $query->fetchall();
+		return $this->objetos;
+	}
+}
